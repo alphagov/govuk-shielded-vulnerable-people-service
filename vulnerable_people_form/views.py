5add06fa97602130fa6b4d7cd26345aa704f86fb
Dan Williams <dan@ithium.net>
2020-07-15 10:31:08 +0100

    Postcode entry/lookup page
    This adds the postcode entry page (for the lookup).

--- vulnerable_people_form/views.py
+++ vulnerable_people_form/views.py
@@ -1,5 +1,6 @@
 import datetime
 import enum
+import re
 
 from flask import current_app, Blueprint, render_template, redirect, request, session
 
@@ -302,5 +303,49 @@ def get_date_of_birth():
         previous_path="/name",
         values=form_answers().get("date_of_birth", {}),
         **get_errors_from_session("date_of_birth"),
+    )
+
+
+def validate_postcode(section):
+    postcode = request.form.get("postcode")
+
+    postcode.replace(" ", "")
+    postcode_regex = "(([A-Z]{1,2}[0-9][A-Z0-9]?|ASCN|STHL|TDCU|BBND|[BFS]IQQ|PCRN|TKCA) ?[0-9][A-Z]{2}|BFPO ?[0-9]{1,4}|(KY[0-9]|MSR|VG|AI)[ -]?[0-9]{4}|[A-Z]{2} ?[0-9]{2}|GE ?CX|GIR ?0A{2}|SAN ?TA1)"
+    error = None
+    if not postcode:
+        error = "What is the postcode where you need support?"
+    elif re.match(postcode_regex, postcode.upper()) is None:
+        error = "Enter a real postcode"
+
+    if error:
+        error_section = session.setdefault("error_items", {}).get(section, {})
+        session["error_items"] = {
+            **session.setdefault("error_items", {}),
+            section: {**error_section, "postcode": error},
+        }
+
+    return error is None
+
+
+@form.route("/postcode-lookup", methods=["POST"])
+def post_postcode_lookup():
+    session["form_answers"] = {
+        **session.setdefault("form_answers", {}),
+        "postcode": {**request.form},
+    }
+    if not validate_postcode("postcode"):
+        return redirect("/postcode-lookup")
+
+    session["error_items"] = {}
+    return redirect("/support-address")
+
+
+@form.route("/postcode-lookup", methods=["GET"])
+def get_postcode_lookup():
+    return render_template(
+        "postcode-lookup.html",
+        previous_path="/name",
+        values=form_answers().get("postcode", {}),
+        **get_errors_from_session("postcode"),
     )
 
