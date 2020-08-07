085c3dae098bb98a81fa4cf18597cb545c2bb810
Dan Williams <dan@ithium.net>
2020-08-07 13:04:12 +0100

    Added wrapper methods for persisting the session

--- vulnerable_people_form/form_pages/shared/session.py
+++ vulnerable_people_form/form_pages/shared/session.py
@@ -3,6 +3,8 @@ from flask import request, session
 from .answers_enums import NHSLetterAnswers
 from .constants import PAGE_TITLES
 
+from ...integrations import persistence
+
 
 def form_answers():
     return session.setdefault("form_answers", {})
@@ -139,3 +141,79 @@ def get_summary_rows_from_form_answers():
         summary_rows.append(row)
 
     return summary_rows
+
+
+def persist_answers_from_session():
+    args = [
+        form_answers()["nhs_number"],
+        form_answers()["nhs_letter"],
+        form_answers()["name"]["first_name"],
+        form_answers()["name"]["middle_name"],
+        form_answers()["name"]["last_name"],
+        form_answers()["address"]["address_line1"],
+        form_answers()["address"]["address_town_city"],
+        form_answers()["address"]["address_postcode"],
+        form_answers()["address"]["address_county"],
+        form_answers()["address"]["address_uprn"],
+        form_answers()["contact_details"]["phone_number_calls"],
+        form_answers()["contact_details"]["phone_number_texts"],
+        form_answers()["contact_details"]["email"],
+        form_answers()["essential_supplies"],
+        form_answers()["medical_conditions"],
+        form_answers()["applying_on_own_behalf"],
+        form_answers()["date_of_birth"],
+    ]
+    kwargs = {
+        "carry_supplies": form_answers().get("carry_supplies"),
+        "uid_nhs_login": session.get("nhs_sub"),
+        "address_line2": form_answers().get("address_line2"),
+        "dietary_requirements": form_answers().get("dietary_requirements"),
+    }
+    form_uid = session.get("form_uid")
+    if form_uid is None:
+        session["form_uid"] = persistence.save_answers(*args, **kwargs)
+    else:
+        persistence.update_answers(form_uid, *args, **kwargs)
+    return session["form_uid"]
+
+
+def load_answers_into_session_if_available():
+    nhs_sub = session.get("nhs_sub")
+    if nhs_sub is None:
+        raise RuntimeError("Could not find nhs_sub in session")
+    stored_answers = persistence.load_answers(nhs_sub)
+    if stored_answers:
+        session["form_answers"] = {
+            "nhs_number": stored_answers["nhs_number"],
+            "nhs_letter": stored_answers["nhs_letter"],
+            "name": {
+                "first_name": stored_answers["first_name"],
+                "middle_name": stored_answers["middle_name"],
+                "last_name": stored_answers["last_name"],
+            },
+            "postcode": stored_answers["address_postcode"],
+            "address": {
+                "address_line1": stored_answers["address_line1"],
+                "address_line2": stored_answers["address_line2"],
+                "town_city": stored_answers["town_city"],
+                "county": stored_answers["county"],
+                "uprn": stored_answers["uprn"],
+            },
+            "contact_details": {
+                "phone_number_calls": stored_answers["contact_number_calls"],
+                "phone_number_texts": stored_answers["contact_number_texts"],
+                "email": stored_answers["contact_email"],
+            },
+            "essential_supplies": stored_answers["essential_supplies"],
+            "medical_conditions": stored_answers["medical_conditions"],
+            "applying_on_own_behalf": stored_answers["applying_on_own_behalf"],
+            "date_of_birth": stored_answers["date_of_birth"],
+            "carry_supplies": stored_answers["carry_supplies"],
+            "dietary_requirements": stored_answers["dietary_requirements"],
+            "basic_care_needs": stored_answers["basic_care_needs"],
+        }
+        session["nhs_sub"] = (stored_answers["nhs_uid"],)
+        session["accessing_saved_answers"] = True
+        return True
+    return False
+
