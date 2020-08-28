576feb94f345154fe4b69abd1d1e3cc731288c7f
Sandeep Nagra <san@initialspark.co.uk>
2020-08-28 17:46:54 +0100

    Updated name of SPL sproc

--- vulnerable_people_form/integrations/spl_check.py
+++ vulnerable_people_form/integrations/spl_check.py
@@ -3,7 +3,7 @@ from .persistence import generate_string_parameter, generate_date_parameter, exe
 
 def check_spl(nhs_number, date_of_birth):
     records = execute_sql(
-        "CALL cv_base.is_person_on_the_spl(:nhs_number, :date_of_birth)",
+        "CALL cv_base.is_person_currently_on_the_spl(:nhs_number, :date_of_birth)",
         (
             generate_string_parameter("nhs_number", nhs_number),
             generate_date_parameter("date_of_birth", date_of_birth),
