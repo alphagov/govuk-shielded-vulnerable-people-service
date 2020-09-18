c05e311ac2de6077bdab91d922d5e05dc56e54c8
Sandeep Nagra <san@initialspark.co.uk>
2020-09-18 10:54:44 +0100

    Fix linting issue

--- vulnerable_people_form/form_pages/shared/validation.py
+++ vulnerable_people_form/form_pages/shared/validation.py
@@ -287,7 +287,7 @@ def validate_email_if_present(section_key, email_key):
     if email_address:
         try:
             email_validator.validate_email(email_address)
-        except email_validator.EmailNotValidError as e:
+        except email_validator.EmailNotValidError:
             error_message = "Enter an email address in the correct format, like name@example.com"
             error_section = session.setdefault("error_items", {}).get(section_key, {})
             session["error_items"] = {
