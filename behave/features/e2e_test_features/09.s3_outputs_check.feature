c07a32e03f3a3de8f40fb24c2b00f9c584417b2d
Junaid Sharif <junaid.sharif@gmail.com>
2020-10-29 17:38:03 +0000

    Address PR comments
    This push makes following changes
    - Modify feature file to re-word behave test cases
    - Modify implementation to
     -- Use strftime instead of str to get the definitive date format
     -- Made the functions more generic to be able to be used for both LA and supermarket feed.

--- behave/features/e2e_test_features/09.s3_outputs_check.feature
+++ behave/features/e2e_test_features/09.s3_outputs_check.feature
@@ -2,7 +2,7 @@
 @s3_outputs_check
 Feature: COVID-19 Shielded vulnerable people service - validate data from previous day
     Scenario: Pipeline should be executed and data validated for previous day run
-        Given the data is available for yesterday
-        When the pipeline is processed
+        Given there was a new web submission yesterday
+        When the pipeline has completed successfully
         Then nhs_number "9999999999" is available in LA feed for "blackburn-with-darwen" for yesterday
         AND nhs_number "9999999999" is available in supermarket feed for yesterday
--- behave/features/e2e_test_features/pipeline_validation_steps/pipeline_validation_steps.py
+++ behave/features/e2e_test_features/pipeline_validation_steps/pipeline_validation_steps.py
@@ -14,7 +14,7 @@ s3 = boto3.client('s3')
 glue_client = boto3.client('glue', region_name=region)
 
 
-@given('the data is available for yesterday')
+@given('there was a new web submission yesterday')
 def the_data_is_available_for_yesterday(context):
     '''
     This will always be true. The data is inserted in Yesterday's run since pipeline needs to cut-off data before
@@ -23,82 +23,66 @@ def the_data_is_available_for_yesterday(context):
     pass
 
 
-@when('the pipeline is processed')
+@when('the pipeline has completed successfully')
 def run_daily_pipeline(context):
     '''
-    This will always be true as well. Pipeline is run in staging as part of Gatling stress test.
+    This will always be true as well. Pipeline is run in staging as part of Daily pipeline and MI execution test.
     '''
     pass
 
 
 @then('nhs_number "{nhs_number}" is available in LA feed for "{hub}" for yesterday')
 def assert_nhs_number_is_available_in_hub(context, nhs_number, hub):
-    la_results = get_la_result_for_user(nhs_number, hub)
+    bucket = 'gds-ons-covid-19-query-results-{}'.format(env)
+    prefix = 'web-app-{}-data/local_authority/{}/'.format(env, hub)
+    filename_prefix = 'GDS-to-LA-Submissions'
+    id_field = 'nhs_number'
+    timestamp_field = 'submission_datetime'
+
+    la_results = get_result_for_user(nhs_number, id_field, bucket, prefix, filename_prefix, timestamp_field)
     assert len(la_results) == 1
 
 
 @then('nhs_number "{nhs_number}" is available in supermarket feed for yesterday')
 def assert_nhs_number_is_available_in_supermarket(context, nhs_number):
-    supermarket_results = get_supermarket_result_for_user(nhs_number)
-    assert len(supermarket_results) == 1
+    bucket = 'gds-ons-covid-19-query-results-{}'.format(env)
+    prefix = 'web-app-{}-data/supermarket/feed/'.format(env)
+    filename_prefix = 'GDS-to-Supermarkets-Submissions'
+    id_field = 'ID'
+    timestamp_field = 'Timestamp'
+    id = hash_nhs_number(nhs_number)
+
+    la_results = get_result_for_user(id, id_field, bucket, prefix, filename_prefix, timestamp_field)
+    assert len(la_results) == 1
 
 
-def get_la_result_for_user(nhs_number, hub):
-    la_results = get_latest_la_file_contents(hub)
-    print("Using NHS number: {}".format(nhs_number))
+def get_result_for_user(id, id_field, bucket, prefix, filename_prefix, timestamp_field):
+    la_results = get_latest_file_contents(bucket, prefix, filename_prefix)
+    print("Using {}: {}".format(id_field, id))
+
+    date_stamp = (date.today() - timedelta(days=1)).strftime('%Y-%m-%d')
+    print("Checking timestamp {}".format(date_stamp))
+
     user_results = []
     for result in la_results:
-        if result['nhs_number'] == nhs_number and \
-                str(date.today() - timedelta(days=1)) in result['submission_datetime']:
+        if result[id_field] == id and date_stamp in result[timestamp_field]:
             user_results.append(result)
     return user_results
 
 
-def get_latest_la_file_contents(hub):
-    bucket = 'gds-ons-covid-19-query-results-{}'.format(env)
-    prefix = 'web-app-{}-data/local_authority/{}/'.format(env, hub)
+def get_latest_file_contents(bucket, prefix, filename_prefix):
     print('Prefix is :%s' % (prefix))
-    filenames = [obj['Key'] for obj in s3.list_objects(
+    filenames = [obj['Key'] for obj in s3.list_objects_v2(
         Bucket=bucket, Prefix=prefix)['Contents']]
 
-    # find the latest file with GDS-to-LA-Submissions' in filename
+    # find the latest file with filename prefix in filename
     # e.g. web-app-{env}-data/local_authority/Barnsley/2020/10/10/GDS-to-LA-Submissions20200917-162656.csv
+    # e.g. web-app-{env}-data/supermarket/feed/GDS-to-Supermarkets-Submissions20200917-162656.csv
     for file in filenames:
-        if 'GDS-to-LA-Submissions' in file:
+        if filename_prefix in file:
             latest_file = file
 
     print("Using file: s3://{}/{}".format(bucket, latest_file))
-    response = s3.get_object(Bucket=bucket, Key=latest_file)
-    lines = response['Body'].read().decode('utf-8').splitlines()
-    return csv.DictReader(lines)
-
-
-def get_supermarket_result_for_user(nhs_number):
-    supermarket_results = get_latest_supermarket_file_contents()
-    id = hash_nhs_number(nhs_number)
-    print("Using NHS number: {} with id: {}".format(nhs_number, id))
-    user_results = []
-    for result in supermarket_results:
-        if result['ID'] == id and \
-                str(date.today() - timedelta(days=1)) in result['Timestamp']:
-            user_results.append(result)
-    return user_results
-
-
-def get_latest_supermarket_file_contents():
-    # get latest file from s3 gds-ons-covid-19-query-results-{env}/web-app-{env}-data/wholesaler/v2
-    bucket = 'gds-ons-covid-19-query-results-{}'.format(env)
-    prefix = 'web-app-{}-data/supermarket/feed/'.format(env)
-    filenames = [obj['Key'] for obj in s3.list_objects(
-        Bucket=bucket, Prefix=prefix)['Contents']]
-
-    # find the latest file with 'GDS-to-Supermarkets-Submissions' in filename
-    # e.g. web-app-{env}-data/supermarket/feed/GDS-to-Supermarkets-Submissions20200917-162656.csv
-    for file in filenames:
-        if 'GDS-to-Supermarkets-Submissions' in file:
-            latest_file = file
-
-    print("Using file: {}".format(latest_file))
     response = s3.get_object(Bucket=bucket, Key=latest_file)
     lines = response['Body'].read().decode('utf-8').splitlines()
     return csv.DictReader(lines)
