1ab9f33f3903006fc24130025540bb2d55c2ef9b
Sandeep Nagra <san@initialspark.co.uk>
2020-11-06 09:22:25 +0000

    Remove back link from nhs registration page
    Update the design to match figma as this page is not designed to have a back button

--- vulnerable_people_form/form_pages/nhs_registration.py
+++ vulnerable_people_form/form_pages/nhs_registration.py
@@ -1,19 +1,14 @@
 from flask import current_app
 
 from .blueprint import form
-from .shared.answers_enums import YesNoAnswers, get_radio_options_from_enum
 from .shared.constants import JourneyProgress
-from .shared.querystring_utils import append_querystring_params
 from .shared.render import render_template_with_title
-from .shared.session import form_answers, get_errors_from_session
+from .shared.session import get_errors_from_session
 
 
 @form.route("/nhs-registration", methods=["GET"])
 def get_nhs_registration():
     return render_template_with_title(
         "nhs-registration.html",
-        radio_items=get_radio_options_from_enum(YesNoAnswers, form_answers().get("nhs_registration")),
         nhs_registration_href=current_app.nhs_oidc_client.get_registration_url(JourneyProgress.NHS_REGISTRATION),
-        previous_path=append_querystring_params("/basic-care-needs"),
-        **get_errors_from_session("nhs_registration"),
     )
