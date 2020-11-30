1fe132e33388f8c19821905cdd16e2803d9f7fa7
tal-nagra <tal@initialspark.co.uk>
2020-11-30 13:20:39 +0000

    Add dynamic journey business logic unit tests
    Ensures the new dynamic journey functionality has
    sufficient unit test coverage.

--- tests/integrations/test_postcode_eligibility.py
+++ tests/integrations/test_postcode_eligibility.py
@@ -2,7 +2,8 @@ from unittest.mock import patch
 
 import pytest
 
-from vulnerable_people_form.integrations.postcode_eligibility import check_postcode, get_ladcode
+from vulnerable_people_form.form_pages.shared.constants import PostcodeTier
+from vulnerable_people_form.integrations.postcode_eligibility import check_postcode, get_ladcode, get_postcode_tier
 
 
 @pytest.mark.parametrize("stored_proc_return_value, expected_output",
@@ -37,3 +38,43 @@ def test_get_ladcode_should_return_valid_ladcode_if_postcode_found(
                return_value={"records": [[{"stringValue": stored_proc_return_value}]]}):
         ladcode = get_ladcode("B21 1BA")
         assert ladcode == expected_output
+
+
+def test_get_postcode_tier_should_return_none_when_ladcode_not_found():
+    postcode = "LS11BA"
+    with patch("vulnerable_people_form.integrations.postcode_eligibility.get_ladcode",
+               return_value=None) as mock_get_ladcode:
+        postcode_tier = get_postcode_tier(postcode)
+        assert mock_get_ladcode.called_once_with(postcode)
+        assert postcode_tier is None
+
+
+def test_get_postcode_tier_should_invoke_get_tier_by_ladcode_when_ladcode_retrieved():
+    postcode = "LS11BA"
+    ladcode = "E08000014"
+    postcode_tier = PostcodeTier.VERY_HIGH.value
+
+    with patch("vulnerable_people_form.integrations.postcode_eligibility.get_ladcode",
+               return_value=ladcode) as mock_get_ladcode, \
+        patch("vulnerable_people_form.integrations.postcode_eligibility.get_tier_by_ladcode",
+              return_value=postcode_tier) as mock_get_tier_by_ladcode:
+        postcode_tier = get_postcode_tier(postcode)
+        assert mock_get_ladcode.called_once_with(postcode)
+        assert mock_get_tier_by_ladcode.called_once_with(ladcode)
+        assert postcode_tier == postcode_tier
+
+
+def test_get_postcode_tier_should_raise_a_value_error_when_an_unexpected_postcode_tier_is_retrieved():
+    postcode = "LS11BA"
+    ladcode = "E08000014"
+    postcode_tier = 8
+
+    with patch("vulnerable_people_form.integrations.postcode_eligibility.get_ladcode",
+               return_value=ladcode) as mock_get_ladcode, \
+        patch("vulnerable_people_form.integrations.postcode_eligibility.get_tier_by_ladcode",
+              return_value=postcode_tier) as mock_get_tier_by_ladcode, \
+            pytest.raises(ValueError) as err_info:
+        get_postcode_tier(postcode)
+        assert mock_get_ladcode.called_once_with(postcode)
+        assert mock_get_tier_by_ladcode.called_once_with(ladcode)
+    assert str(err_info.value) == f"Unexpected postcode tier retrieved: {postcode_tier}"
