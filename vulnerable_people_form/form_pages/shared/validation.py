66505fa0b9bbc128ecc8b9f7535a5165a2403bed
Tom Ridd <twridd@gmail.com>
2020-12-18 11:39:22 +0000

    fix error thrown in logging - add specific msg to InvalidPhoneError

--- vulnerable_people_form/form_pages/shared/validation.py
+++ vulnerable_people_form/form_pages/shared/validation.py
@@ -319,7 +319,7 @@ def phone_number_is_valid_for_notify(phone_number, logger=None):
     except InvalidPhoneError as e:
         if logger:
             logger.warning(create_log_message(log_event_names["NOT_VALID_PHONE_NUMBER_FOR_TEXTS_ENTERED"],
-                                              f"Invalid phone for receiving Notify SMS entered: {e.message}"))
+                                              f"Invalid phone for receiving Notify SMS entered: {e.msg}"))
         return False
 
 
